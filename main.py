25f896093f68ec9fa9f26fb6f4f7ab08851793d2
Rachel <boisedoll@hotmail.com>
2021-06-12 23:08:49 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from sqlalchemy import Column, String, Boolean, Date
+from send_text import send_reminder, request_approval
+import os
+
+app = Flask(__name__)
+# Setup db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("sqlite_todo_db")
+# Silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# CREATE TABLE
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    due_date = db.Column(db.Date)
+    pending_completion = db.Column(db.Boolean)
+    complete = db.Column(db.Boolean)
+
+
+# db.create_all()
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    todos = Todo.query.order_by(Todo.due_date).all()
+    return render_template("index.html", todos=todos)
+
+
+@app.route('/add', methods=["POST"])
+def add_task():
+    get_due_date = request.form["date"]
+    due_date = datetime.datetime.strptime(get_due_date, "%Y-%m-%d")
+    todo = Todo(name=request.form["task"].capitalize(), due_date=due_date, pending_completion=False, complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/mark_complete', methods=["POST", "GET"])
+def click_complete():
+    todo_id = request.args.get("id")
+    todo = Todo.query.get(todo_id)
+    todo.pending_completion = True
+    db.session.commit()
+    request_approval(todo.name)
+    return redirect(url_for('home'))
+
+
+all_todos = Todo.query.all()
+todos_string='\n'.join([todo.name for todo in all_todos])
+send_reminder(todos_string)
+
+if __name__ == '__main__':
+    app.run(debug=True)
